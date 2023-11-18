ce22de1c2a98a70bee9dab225f060364d94de672
Abhijeet Krishnan <abhijeet.krishnan@gmail.com>
2023-11-18 16:59:43 -0500

    Add abstract class to handle sending requests

--- src/codeforces/methods.py
+++ src/codeforces/methods.py
@@ -1,49 +1,83 @@
-from typing import List, Tuple, TypedDict
+from typing import Any, Dict, List, Optional, Tuple, Type, TypedDict
 
 import requests
+from objects import (BlogEntry, Comment, Contest, Hack, Problem,
+                     ProblemStatistics, RanklistRow, RatingChange,
+                     RecentAction, Submission, User)
+from pydantic import BaseModel, create_model
+from requests.exceptions import ConnectionError, HTTPError, Timeout
 
-from .objects import (BlogEntry, Comment, Contest, Hack, Problem,
-                      ProblemStatistics, RanklistRow, RatingChange,
-                      RecentAction, Submission, User)
-
-# TODO: better way to use the requests library to pass args rather than forming
-# a string template?
-CF_API = 'https://codeforces.com/api/{method}'
-
-
-def getBlogEntryComments(blogEntryId: int) -> List[Comment]:
-    METHOD = f'blogEntry.comments?blogEntryId={blogEntryId}'
-    pass
+DEFAULT_TIMEOUT = 9.01
+CF_API = 'https://codeforces.com/api'
 
 
-def getBlogEntryView(blogEntryId: int) -> BlogEntry:
-    METHOD = f'blogEntry.view?blogEntryId={blogEntryId}'
-    pass
+class ApiMethod:
+
+    def __init__(self, methodName: str, payloadSchema: Dict[str, Any], returnSchema: Tuple[Type, Any], baseUrl: str = CF_API, timeout: float = DEFAULT_TIMEOUT):
+        self._methodName = methodName
+        self._url = f'{baseUrl}/{methodName}'
+        self._payloadModel = create_model(methodName, **payloadSchema)
+        self._returnModel = create_model(
+            f'return_{methodName}', status=str, result=returnSchema)
+        self._timeout = timeout
+
+    def makeRequest(self, payload: Dict[str, Any]) -> Optional[BaseModel]:
+        try:
+            _ = self._payloadModel(**payload)
+            r = requests.get(self._url,
+                             payload, timeout=self._timeout)
+            if r.status_code == 200:
+                returnValue = self._returnModel(**r.json())
+                return returnValue.result
+            else:
+                r.raise_for_status()
+        except ValueError as v:
+            print(f'Invalid payload {payload} to {self._methodName}: {v}')
+        except Timeout:
+            print(
+                f'Timeout on request to {self._methodName} after {self._timeout} seconds.')
+        except ConnectionError:
+            print(f'Connection error on request to {self._methodName}.')
+        except HTTPError as err:
+            print(
+                f'Unsuccessful response code {err} on request to {self._methodName}. Response recieved is {r.json()}')
+        return None
 
 
-def getContestHacks(contestId: int, asManager: bool = False) -> List[Hack]:
-    METHOD = f'contest.hacks?contestId={contestId}&asManager={asManager}'
-    pass
-
-
-def getContestList(gym: bool = False) -> List[Contest]:
-    METHOD = f'contest.list?gym={gym}'
-    pass
-
-
-def getContestRatingChanges(contestId: int) -> List[RatingChange]:
-    METHOD = f'contest.ratingChanges?contestId={contestId}'
-    pass
-
-
-class ContestStandings(TypedDict):
+class ContestStandings(BaseModel):
     contest: Contest
     problems: List[Problem]
     rows: List[RanklistRow]
 
 
-def getContestStandings(contestId: int, from_: int, count: int, handles: List[str], room: str = None, showUnofficial: bool = False, asManager: bool = False) -> ContestStandings:
-    pass
+# TODO: what's the most ergonomic way to set this up?
+CF_API_METHODS = {
+    'getBlogEntryComments': ApiMethod('blogEntry.comments', {
+        'blogEntryId': (int, ...)
+    }, (List[Comment], ...)),
+    'getBlogEntryView': ApiMethod('blogEntry.view', {
+        'blogEntryId': (int, ...)
+    }, (BlogEntry, ...)),
+    'getContestHacks': ApiMethod('contest.hacks', {
+        'contestId': (int, ...),
+        'asManager': (bool, None)
+    }, (List[Hack], ...)),
+    'getContestList': ApiMethod('contest.list', {
+        'gym': (bool, None)
+    }, (List[Contest], ...)),
+    'getContestRatingChanges': ApiMethod('contest.ratingChanges', {
+        'contestId': (int, ...)
+    }, (List[RatingChange], ...)),
+    'getContestStandings': ApiMethod('contest.standings', {
+        'contestId': (int, ...),
+        'asManager': (bool, None),
+        'from': (int, None),
+        'count': (int, None),
+        'handles': (str, None),
+        'room': (int, None),
+        'showUnofficial': (bool, None)
+    }, (ContestStandings, ...))
+}
 
 
 def getContestStatus(contestId: int, handle: str, from_: int, count: int, asManager: bool = False) -> List[Submission]:
@@ -84,3 +118,15 @@ def getUserRating(handle: str) -> List[RatingChange]:
 
 def getUserStatus(handle: str, from_: int, count: int) -> List[Submission]:
     pass
+
+
+def main():
+    standings = CF_API_METHODS['getContestStandings'].makeRequest(
+        {'contestId': 566, 'from': 1, 'count': 5})
+    print(standings)
+    import code
+    code.interact(local=locals())
+
+
+if __name__ == '__main__':
+    main()
--- src/codeforces/objects.py
+++ src/codeforces/objects.py
@@ -6,6 +6,8 @@ from pydantic import (BaseModel, Field, HttpUrl, NonNegativeFloat,
 
 
 class User(BaseModel):
+    "Represents a Codeforces user."
+
     handle: str
     email: str
     vkId: str
@@ -28,6 +30,8 @@ class User(BaseModel):
 
 
 class BlogEntry(BaseModel):
+    "Represents a Codeforces blog entry. May be in either short or full version."
+
     id: int
     originalLocale: str
     creationTimeSeconds: NonNegativeInt
@@ -42,6 +46,8 @@ class BlogEntry(BaseModel):
 
 
 class Comment(BaseModel):
+    "Represents a comment."
+
     id: int
     creationTimeSeconds: NonNegativeInt
     commentatorHandle: str
@@ -52,12 +58,16 @@ class Comment(BaseModel):
 
 
 class RecentAction(BaseModel):
+    "Represents a recent action."
+
     timeSeconds: NonNegativeInt
     blogEntry: BlogEntry
     comment: Comment
 
 
 class RatingChange(BaseModel):
+    "Represents a participation of user in rated contest."
+
     contestId: int
     contestName: str
     handle: str
@@ -68,20 +78,22 @@ class RatingChange(BaseModel):
 
 
 class ContestType(str, Enum):
-    CF = auto()
-    IOI = auto()
-    ICPC = auto()
+    CF = 'CF'
+    IOI = 'IOI'
+    ICPC = 'ICPC'
 
 
 class ContestPhase(str, Enum):
-    BEFORE = auto()
-    CODING = auto()
-    PENDING_SYSTEM_TEST = auto()
-    SYSTEM_TEST = auto()
-    FINISHED = auto()
+    BEFORE = 'BEFORE'
+    CODING = 'CODING'
+    PENDING_SYSTEM_TEST = 'PENDING_SYSTEM_TEST'
+    SYSTEM_TEST = 'SYSTEM_TEST'
+    FINISHED = 'FINISHED'
 
 
 class Contest(BaseModel):
+    "Represents a contest on Codeforces."
+
     id: int
     name: str
     type: ContestType
@@ -102,73 +114,99 @@ class Contest(BaseModel):
     season: Optional[str]
 
 
-class Party(BaseModel):
-    pass
+class ParticipantType(str, Enum):
+    CONTESTANT = 'CONTESTANT'
+    PRACTICE = 'PRACTICE'
+    VIRTUAL = 'VIRTUAL'
+    MANAGER = 'MANAGER'
+    OUT_OF_COMPETITION = 'OUT_OF_COMPETITION'
 
 
 class Member(BaseModel):
-    pass
+    "Represents a member of a party."
+
+    handle: str
+    name: Optional[str]
+
+
+class Party(BaseModel):
+    "Represents a party, participating in a contest."
+
+    contestId: Optional[int]
+    members: List[Member]
+    participantType: ParticipantType
+    teamId: Optional[int]
+    teamName: Optional[str]
+    ghost: bool
+    room: Optional[int]
+    startTimeSeconds: Optional[NonNegativeInt]
 
 
 class ProblemType(str, Enum):
-    PROGRAMMING = auto()
-    QUESTION = auto()
+    PROGRAMMING = 'PROGRAMMING'
+    QUESTION = 'QUESTION'
 
 
 class Problem(BaseModel):
+    "Represents a problem."
+
     contestId: Optional[int]
     problemsetName: Optional[str]
     index: str
     name: str
     type: ProblemType
     points: Optional[PositiveFloat]
-    rating: Optional[int] = Field(ge=1, le=5)
+    rating: Optional[NonNegativeInt]
     tags: List[str]
 
 
 class ProblemStatistics(BaseModel):
+    "Represents a statistic data about a problem."
+
     contestId: Optional[int]
     index: str
     solvedCount: NonNegativeInt
 
 
 class Verdict(str, Enum):
-    FAILED = auto()
-    OK = auto()
-    PARTIAL = auto()
-    COMPILATION_ERROR = auto()
-    RUNTIME_ERROR = auto()
-    WRONG_ANSWER = auto()
-    PRESENTATION_ERROR = auto()
-    TIME_LIMIT_EXCEEDED = auto()
-    MEMORY_LIMIT_EXCEEDED = auto()
-    IDLENESS_LIMIT_EXCEEDED = auto()
-    SECURITY_VIOLATED = auto()
-    CRASHED = auto()
-    INPUT_PREPARATION_CRASHED = auto()
-    CHALLENGED = auto()
-    SKIPPED = auto()
-    TESTING = auto()
-    REJECTED = auto()
+    FAILED = 'FAILED'
+    OK = 'OK'
+    PARTIAL = 'PARTIAL'
+    COMPILATION_ERROR = 'COMPILATION_ERROR'
+    RUNTIME_ERROR = 'RUNTIME_ERROR'
+    WRONG_ANSWER = 'WRONG_ANSWER'
+    PRESENTATION_ERROR = 'PRESENTATION_ERROR'
+    TIME_LIMIT_EXCEEDED = 'TIME_LIMIT_EXCEEDED'
+    MEMORY_LIMIT_EXCEEDED = 'MEMORY_LIMIT_EXCEEDED'
+    IDLENESS_LIMIT_EXCEEDED = 'IDLENESS_LIMIT_EXCEEDED'
+    SECURITY_VIOLATED = 'SECURITY_VIOLATED'
+    CRASHED = 'CRASHED'
+    INPUT_PREPARATION_CRASHED = 'INPUT_PREPARATION_CRASHED'
+    CHALLENGED = 'CHALLENGED'
+    SKIPPED = 'SKIPPED'
+    TESTING = 'TESTING'
+    REJECTED = 'REJECTED'
 
 
 class TestSet(str, Enum):
-    SAMPLES = auto()
-    PRETESTS = auto()
-    TESTS = auto()
-    TESTS1 = auto()
-    TESTS2 = auto()
-    TESTS3 = auto()
-    TESTS4 = auto()
-    TESTS5 = auto()
-    TESTS6 = auto()
-    TESTS7 = auto()
-    TESTS8 = auto()
-    TESTS9 = auto()
-    TESTS10 = auto()
+    SAMPLES = 'SAMPLES'
+    PRETESTS = 'PRETESTS'
+    TESTS = 'TESTS'
+    TESTS1 = 'TESTS1'
+    TESTS2 = 'TESTS2'
+    TESTS3 = 'TESTS3'
+    TESTS4 = 'TESTS4'
+    TESTS5 = 'TESTS5'
+    TESTS6 = 'TESTS6'
+    TESTS7 = 'TESTS7'
+    TESTS8 = 'TESTS8'
+    TESTS9 = 'TESTS9'
+    TESTS10 = 'TESTS10'
 
 
 class Submission(BaseModel):
+    "Represents a submission."
+
     id: int
     contestId: Optional[int]
     creationTimeSeconds: Optional[NonNegativeInt]
@@ -184,13 +222,55 @@ class Submission(BaseModel):
     points: Optional[NonNegativeFloat]
 
 
+class HackVerdict(str, Enum):
+    HACK_SUCCESSFUL = 'HACK_SUCCESSFUL'
+    HACK_UNSUCCESSFUL = 'HACK_UNSUCCESSFUL'
+    INVALID_INPUT = 'INVALID_INPUT'
+    GENERATOR_INCOMPILABLE = 'GENERATOR_INCOMPILABLE'
+    GENERATOR_CRASHED = 'GENERATOR_CRASHED'
+    IGNORED = 'IGNORED'
+    TESTING = 'TESTING'
+    OTHER = 'OTHER'
+
+
+class JudgeProtocol(BaseModel):
+    manual: bool
+    protocol: str
+    verdict: HackVerdict
+
+
 class Hack(BaseModel):
-    pass
+    "Represents a hack, made during Codeforces Round."
+
+    id: int
+    creationTimeSeconds: NonNegativeInt
+    hacker: Party
+    defender: Party
+    verdict: Optional[HackVerdict]
+    problem: Problem
+    test: Optional[str]
+    judgeProtocol: Optional[JudgeProtocol]
 
 
-class RanklistRow(BaseModel):
-    pass
+class ProblemResultType(str, Enum):
+    PRELIMINARY = 'PRELIMINARY'
+    FINAL = 'FINAL'
 
 
 class ProblemResult(BaseModel):
-    pass
+    points: NonNegativeFloat
+    penalty: Optional[int]
+    rejectedAttemptCount: NonNegativeInt
+    type: ProblemResultType
+    bestSubmissionTimeSeconds: Optional[NonNegativeInt]
+
+
+class RanklistRow(BaseModel):
+    party: Party
+    rank: PositiveInt
+    points: NonNegativeFloat
+    penalty: int
+    successfulHackCount: NonNegativeInt
+    unsuccessfulHackCount: NonNegativeInt
+    problemResults: List[ProblemResult]
+    lastSubmissionTimeSeconds: Optional[NonNegativeInt]
